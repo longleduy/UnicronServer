d8f6f474a42f9a9276662180a4c5da022d6d72a8
LongLD <longdd21081994@gmail.com>
2019-03-23 17:39:54 +0700

    1

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose'
 export const connectMongooseDB = async () => {
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('useNewUrlParser', true);
-    mongoose.connect(process.env.MONGODB_PATH, { useNewUrlParser: true });
+    try {
+        mongoose.set('useCreateIndex', true);
+        mongoose.set('useFindAndModify', false);
+        mongoose.set('useNewUrlParser', true);
+        mongoose.connect(process.env.MONGODB_PATH, { useNewUrlParser: true });
+    } catch (error) {
+        console.log("MongooseError: "+error);
+    }
+
 }
 
 
